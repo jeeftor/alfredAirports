b57c3c528eabf87520ee6c555b759bb783b23bda
Jeff Stein <jstein@mitre.org>
2016-12-19 15:28:57 -0500

    Added a release.py file into the mix - thats kind of cool no

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
-OUTPUT=$(python build/packageWorkflow.py . -o build/)
+#OUTPUT=$(python build/packageWorkflow.py . -o build/)
+OUTPUT="Airport_Search Airport_Search-0.6.22.alfredworkflow 0.6.22"
+
 TEXT_ARRRAY=($OUTPUT)
+
 
 echo ${TEXT_ARRRAY[0]}
 echo ${TEXT_ARRRAY[1]}
 echo ${TEXT_ARRRAY[2]}
 
 
+# Do you want to git?
 
 # https://github.com/repos/jeeftor/alfredAirports/releases
 
@@ -20,4 +24,24 @@ echo ${TEXT_ARRRAY[2]}
 #   "prerelease": false
 # }
 
+echo "     ________                               __   _______ __ "
+echo "    / ____/ /_  ____ _____  ____ ____  ____/ /  / ____(_) /__  _____ "
+echo "   / /   / __ \/ __ \`/ __ \/ __ \`/ _ \/ __  /  / /_  / / / _ \/ ___/ "
+echo "  / /___/ / / / /_/ / / / / /_/ /  __/ /_/ /  / __/ / / /  __(__  ) "
+echo "  \____/_/ /_/\__,_/_/ /_/\__, /\___/\__,_/  /_/   /_/_/\___/____/ "
+echo "                         /____/ "
+echo ""
 
+git status -s
+
+echo " Commit and make a release?"
+
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+    echo "DOING IT"
+    git add -u
+    git commit -m "Prepping release ${TEXT_ARRRAY[2]}"
+fi
